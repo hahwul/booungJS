9e3d4fe2b96145fa2eb6faf63cb8dfb59cb97fde
hahwul <hahwul@gmail.com>
2017-02-06 22:55:34 +0900

    dev

--- booungJS.js
+++ booungJS.js
@@ -1,4 +1,4 @@
-BooungJS
+//BooungJS
 // This script is security analysis javascript code
 // Load a script !
 // $.getScript("http://hahwul.com/1.js");
@@ -9,20 +9,32 @@ BooungJS
 
 function booungJS()
 {
- // init..
- var scr_list = document.scripts;
- var scr_count = document.scripts.length;	
- // ------
-
- function search()
+	// init..
+	this.scr_list = document.scripts;
+	this.scr_count = document.scripts.length;	
+	// ------
+	 
+	this.scr_buf = [];
+	var i=0;
+	while(i<this.scr_count)
+	{
+		this.scr_buf = this.scr_list[i];
+		i++;
+	}
+ 
+}
+booungJS.prototype.search = function search(searchq)
 	{
 		var i = 0;
-		while(i<=scr_count)
+		console.log("[INF] Searching.. ["+searchq+"]");
+		while(i<this.scr_count)
 		{
-			scr_list[scr_count].innerText;
+			buf = this.scr_list[i].innerText;
+			console.log(this.scr_list[i].text);
+			console.log(i);
+			i++;
 		}
-	}
-}
+	};
 
-
-
+var booung = new booungJS();
+	
