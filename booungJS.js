2a9b242fe4efa25a1261a32744b53be0c2e93e89
HAHWUL <hahwul@gmail.com>
2017-02-06 19:11:49 +0900

    dev

---
+++ booungJS.js
@@ -0,0 +1,28 @@
+BooungJS
+// This script is security analysis javascript code
+// Load a script !
+// $.getScript("http://hahwul.com/1.js");
+
+
+//document.scripts[40].innerText.length
+//document.scripts.length
+
+function booungJS()
+{
+ // init..
+ var scr_list = document.scripts;
+ var scr_count = document.scripts.length;	
+ // ------
+
+ function search()
+	{
+		var i = 0;
+		while(i<=scr_count)
+		{
+			scr_list[scr_count].innerText;
+		}
+	}
+}
+
+
+
